1253b7b716afc089421727aac270051e33f98894
VinicinBraga <vinicius@bragamatta.com.br>
2023-06-06 10:48:30 -0300

    trying to fix error in theme.ts

--- src/expanded-them.ts
+++ src/expanded-them.ts
@@ -1,4 +1,5 @@
-//eslint-disable-next-line
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
